eb98ee2df38fdc283870f34dfd72577057cb8aa0
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-01-03 20:24:11 -0300

    :up:

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,64 @@
-import Menu from "./components/Menu/Menu";
+import { useEffect, useState } from "react";
 import BoxUser from "./components/BoxUser/BoxUser";
-
+import { GoPlus } from "react-icons/go";
 
 function App() {
+  const [user, setNewUser] = useState("");
+  const [dataUser, setNewDataUser] = useState(null);
+
+  const handleNewUser = (e) => {
+    setNewUser(e.target.value);
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${user}`);
+      const data = await response.json();
+      setNewDataUser(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Erro ao buscar usuário:", error);
+      setNewDataUser(null);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetchData();
+  };
 
   return (
     <>
-      <Menu />
-      <div className="flex flex-wrap  p-[15px] gap-[10px] justify-center items-center">
-        <BoxUser />
-        <BoxUser />
-        <BoxUser />
-        <BoxUser addUser={true} />
+      <div className="w-full h-[60px] flex justify-center items-center bg-[#121111] font-sans">
+        <h1 className="font-bold text-[#e8eaea] text-lg">
+          Pesquisar usuários do GitHub
+        </h1>
+      </div>
+      <div className="flex flex-wrap p-[15px] gap-[10px] justify-center items-center font-sans">
+        <div className="flex flex-col justify-center items-center min-w-[150px] w-[250px] p-[10px] rounded-[10px] bg-[#171515] h-[135px] gap-2 font-sans">
+          <form
+            className="flex flex-col gap-2 justify-center items-center"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="user" className="flex flex-col gap-1">
+              <span className="text-white">Usuário:</span>
+              <input
+                type="text"
+                placeholder="Digite o usuário"
+                value={user}
+                onChange={handleNewUser}
+                className="rounded-[4px] outline-none px-1"
+              />
+            </label>
+            <button
+              className="w-[50px] h-[50px] flex justify-center items-center font- rounded-full bg-[#e8eaea] cursor-pointer"
+              type="submit"
+            >
+              <GoPlus size={25} />
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/components/BoxUser/BoxUser.jsx
+++ src/components/BoxUser/BoxUser.jsx
@@ -1,31 +1,14 @@
-import { GoPlus } from "react-icons/go";
 
-const BoxUser = ({ addUser }) => {
-
-  const card = (addUser) => {
-    if (addUser) {
-      return (
-        <div className="flex justify-center items-center min-w-[150px] w-[250px] p-[10px] rounded-[10px] bg-[#171515] h-[135px]">
-          
-          <button className="w-[50px] h-[50px] flex justify-center items-center font- rounded-full bg-[#e8eaea] cursor-pointer">
-            <GoPlus size={25} />
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="min-w-[150px] w-[250px] p-[20px] rounded-[10px] min-h-[100px] h-[135px] flex flex-col gap-2 bg-[#171515]">
+const BoxUser = () => {
+  return (
+    <div className="min-w-[150px] w-[250px] p-[20px] rounded-[10px] min-h-[100px] h-[135px] flex flex-col gap-2 bg-[#171515] font-sans">
           <img src="vite.svg" alt="" className="w-[40px] rounded-full" />
           <div className="text-[#e8eaea]">
             <p>Nome:</p>
             <p>Repositórios:</p>
           </div>
         </div>
-      );
-    }
-  };
-
-  return <div>{card(addUser)}</div>;
+  );
 };
 
 export default BoxUser;
